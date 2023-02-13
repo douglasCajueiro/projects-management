1b71cd3eda9d576330e0c7bf302777b82a60cee5
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2023-02-13 04:08:11 -0300

    Including Users API properties and Controller tags to Swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class CreateUserDto {
 
     readonly id: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     readonly name: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     readonly password: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     readonly username: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
